d1a7b5182617d64d41ef13fa2839d4757a532f90
lucaslomba <lucasazevedolomba@gmail.com>
2020-10-08 11:45:02 -0300

    Quebras de linha

---
+++ strings.php
@@ -0,0 +1,15 @@
+<?php
+
+$idade = 21;
+
+//Usando aspas simples precisa do ponto para concatenar
+echo 'Olá Mundo! Minha idade é '.$idade.' anos';
+
+//Quebra linha
+echo "\n";
+
+//Outro modo de quebrar linha
+echo "Quebrando com End Of Line" . PHP_EOL;
+
+//Usando aspas duplas basta somente colocar o sifrão
+echo "Olá Mundo! Minha idade é $idade anos";
